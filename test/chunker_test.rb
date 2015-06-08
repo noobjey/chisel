9c9a58737bc1e0c2d9a8e85cce50d249f2774ba7
Jason Wright <noobjey@gmail.com>
2015-06-08 11:37:45 -0600

    learned difference between rquire minitest and require minitest slash autorun

--- test/chunker_test.rb
+++ test/chunker_test.rb
@@ -1,10 +1,17 @@
-require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
 require './lib/chunker'
 
 class ChunkerTest < Minitest::Test
 
-  def do_test_work
-    assert false
+  def test_one_line_message_results_in_one_chunk
+    # message = "this is one line"
+    # chunker = Chunker.new
+    #
+    # result = chunker.chunk message
+    #
+    # assert_equal 1, result.length
+    assert true
   end
 
 end
