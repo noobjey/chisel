b7ba054e279df24fe7c898b095a96ab5089daaff
Jason Wright <noobjey@gmail.com>
2015-06-09 16:44:03 -0600

    renderer spit out html, did not keep newlines

--- lib/renderer.rb
+++ lib/renderer.rb
@@ -1,10 +1,33 @@
+require './lib/chunker'
+require './lib/chunk_assigner'
+
 class Renderer
 
   def initialize(input)
-
+    @input = input
   end
 
   def rend
+    chunked = chunk(@input)
+    assigned_chunks = assign(chunked)
+    html = assigned_chunks.map do |chunk|
+      convert_to_html(chunk)
+    end
 
+    html.join
+  end
+
+  def chunk(input)
+    chunker = Chunker.new(input)
+    chunker.chunks
+  end
+
+  def assign(input)
+    chunk_assigner = ChunkAssigner.new(input)
+    chunk_assigner.assign
+  end
+
+  def convert_to_html(input)
+    input.render
   end
 end
--- test/renderer_test.rb
+++ test/renderer_test.rb
@@ -1,22 +1,68 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+
 require './lib/renderer'
+require './lib/header_chunk'
 
 class RendererTest < Minitest::Test
 
   def test_render_chunks_the_input
     input    = '# My Life in Desserts'
-    expected = '<h1>My Life in Dessert</h1>'
-
+    expected = ['# My Life in Desserts']
     renderer = Renderer.new(input)
 
-    assert_equal expected, renderer.chunk
+    assert_equal expected, renderer.chunk(input)
+  end
+
+  def test_render_assigns_chunk_types
+    # skip
+    input    = ['# My Life in Desserts']
+    renderer = Renderer.new(input)
+
+    assigned_chunks = renderer.assign(input)
+
+    assert "Expected Header class got: #{assigned_chunks[0].class} class", assigned_chunks[0].is_a?(HeaderChunk)
+    assert_equal input[0], assigned_chunks[0].text
+  end
+
+  def test_render_ask_chunks_for_html
+    # skip
+    input    = HeaderChunk.new('# i am a header')
+    renderer = Renderer.new(input)
+    expected = '<h1>i am a header</h1>'
+
+    html = renderer.convert_to_html(input)
+
+    assert_equal expected, html
   end
 
   def test_render_input_with_one_header
-    skip
+    # skip
     input    = '# My Life in Desserts'
-    expected = '<h1>My Life in Dessert</h1>'
+    expected = '<h1>My Life in Desserts</h1>'
+
+    renderer = Renderer.new(input)
+
+    assert_equal expected, renderer.rend
+  end
+
+  def test_render_input_with_multiple_chunks
+    skip
+
+    input = <<-the_input
+# My Life in Desserts
+
+"I am a paragraph"
+    the_input
+
+    expected = <<-expected_string
+
+<h1>My Life in Desserts</h1>
+
+<p>
+"I am a paragraph"
+</p>
+    expected_string
 
     renderer = Renderer.new(input)
 
@@ -24,7 +70,7 @@ class RendererTest < Minitest::Test
   end
 
   def test_render_acceptance
-    skip
+    # skip
     input = <<-long_input
 # My Life in Desserts
 
